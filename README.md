45cdfe61fd5f7b3896667a1f73e5fb7ddecf6b54
bxste <97215500+bxste@users.noreply.github.com>
2023-02-26 17:24:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reactive-amg-2135-pb6cbw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/slice-decimal-pipe-amg-2135-pb6cbw-zvvg1z-zbchvh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'pipes reactive';
+  Fruits = ['Apple', ' Orange', ' Grapes', ' Mango', ' Kiwi', ' Pear'];
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+}
